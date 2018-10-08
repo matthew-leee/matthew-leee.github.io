155df83fc3135110a5ecd51c39894ad62ace20b5
matthew-leee <psynergy.lth@gmail.com>
2018-10-08 11:14:01 +0800

    08 C i did it!!!

---
+++ 08-JSON-AJAX/C/C.js
@@ -0,0 +1,24 @@
+function inSpace (callback) {
+    let http = new XMLHttpRequest ();
+
+    http.open("GET", `http://api.open-notify.org/astros.json`)
+
+    http.onreadystatechange = function() {
+        if(http.readyState != XMLHttpRequest.DONE) {
+            return;
+        } else if(http.status == 200) {
+            callback(http.responseText)
+        } else {
+            console.log('error occurred' + http.status);
+        }
+    }
+    
+    http.send();
+}
+
+inSpace (function(data){
+    let jsonData = JSON.parse(data);
+    for (i=0; i<jsonData.people.length; i++){
+        console.log (jsonData.people[i].name);
+    }
+})
