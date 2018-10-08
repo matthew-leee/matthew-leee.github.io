2cebc4936bed922a4038d7b6f7f1f76193ce3c66
matthew-leee <psynergy.lth@gmail.com>
2018-10-08 14:32:40 +0800

    08 dom done

--- 07-jsPrototype-Class/A.js
+++ 07-jsPrototype-Class/A.js
@@ -1,7 +1,21 @@
-function Person (name, age){
-    this.name=name;
-    this.age=age;
+// old syntax
+
+// function Person (name, age){
+//     this.name=name;
+//     this.age=age;
+// }
+
+// let gf = new Person ("Bobo", 22);
+// console.dir (`my gf is ${gf.name}`)
+
+// new syntax
+
+class Person {
+    constructor (option){
+        this.name = option.name;
+        this.age = option.age;
+    }
 }
 
-let gf = new Person ("Bobo", 22);
-console.dir (`my gf is ${gf.name}`)
+let gf = new Person ({name:"Bobo", age: 22})
+console.log (`my gf is ${gf.name} of age ${gf.age}`)
--- 07-jsPrototype-Class/B/B.js
+++ 07-jsPrototype-Class/B/B.js
@@ -1,13 +1,55 @@
-let Player = function (name, health) {
-    this.name = name;
-    this.health = health;
-    this.attack = function (opponent) {
-        opponent.health -= 10;
+// old syntax
+
+// let Player = function (name, health) {
+//     this.name = name;
+//     this.health = health;
+//     this.attack = function (opponent) {
+//         opponent.health -= 10;
+//     }
+// }
+
+// let p1 = new Player("Ariana", 100);
+// let p2 = new Player("Pete", 100);
+
+// function whoAttack(player1, player2) {
+//     let x = Math.random();
+//     if (x < 0.5) {
+//         return player1.attack(player2);
+//     } else {
+//         return player2.attack(player1)
+//     }
+// }
+
+// function play(player1, player2) {
+//     for (i = 0;player1.health > 0 && player2.health > 0; i++) {
+//         whoAttack(player1, player2);
+//         if (player1.health > 0 && player2.health > 0) {
+//             console.log (`${player1.name} health is ${player1.health}, ${player2.name} health is ${player2.health}`)
+//         } else if (player1.health > 0 && player2.health <= 0) {
+//             console.log (`${player1.name} wins`)
+//         } else if (player2.health > 0 && player1.health <= 0) {
+//             console.log (`${player2.name} wins`)
+//         }
+//     }
+
+// }
+
+// console.log (play(p1, p2));
+
+// new syntax 
+
+class Player {
+    constructor (option){
+        this.name = option.name;
+        this.health = option.health;
+    }
+    attack(opponent){
+        opponent.health -=10;
     }
 }
 
-let p1 = new Player("Ariana", 100);
-let p2 = new Player("Pete", 100);
+let p1 = new Player ({name: "Ariana", health: 100})
+let p2 = new Player ({name: "Pete", health: 100})
 
 function whoAttack(player1, player2) {
     let x = Math.random();
