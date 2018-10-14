5e876fb4b74b6e998990def32b064435be49a1ea
matthew-leee <psynergy.lth@gmail.com>
2018-10-15 05:04:20 +0800

    first draft mobile compatibility issues

--- personalPortfo/main.js
+++ personalPortfo/main.js
@@ -1,23 +1,60 @@
 $(function () {
 
     //navBar Hide and Show
-    $(window).on("scroll", function(e){
+    $(window).on("scroll", function (e) {
         let navOld = $(this).scrollTop();
-        console.log ("scroll working")
-        console.log (navOld);
-        let navNew; 
-        setTimeout(function(e){
+        console.log("scroll working")
+        console.log(navOld);
+        let navNew;
+        setTimeout(function (e) {
             navNew = $(this).scrollTop();
             //scrollDown or stop
-            if (navNew >= navOld && navNew > 100){
+            if (navNew >= navOld && navNew > 100) {
                 $("nav").css("background-color", "rgba(255,255,255, 0)")
                 $(".nav-link").css("text-shadow", "0 0 7px #FFFFFF")
-            // scrollup or stay at top 
-            } else if (navNew < navOld || navNew <= 100){
+                // scrollup or stay at top 
+            } else if (navNew < navOld || navNew <= 100) {
                 $("nav").css("background-color", "rgba(255,255,255, 0.5)")
                 $(".nav-link").css("text-shadow", "0 0 0")
-            } 
+            }
         }, 100);
+    })
+
+    //#navbar logo opens on click
+    let clickOpenCount = 0;
+    $("#iconBox img").on("click", function () {
+        clickOpenCount++;
+        if (clickOpenCount <= 1) {
+            $("body").css("overflow", "hidden");
+            $("#nav").animate({
+                height: "+=75vh",
+                backgroundColor: "rgba(255,255,255, 0.7)",
+            }, 400);
+            $("#iconBox, #iconBox img").animate({
+                height: "+=20vh",
+                width: "+=20vh",
+            }, 400);
+        }
+    })
+
+    $(".nav-link").on("click", function () {
+        //navbar collapse
+        if (clickOpenCount == 1) {
+            $("body").css("overflow", "auto");
+            $("#nav").animate({
+                height: "-=75vh",
+                backgroundColor: "rgba(255,255,255, 0.5)",
+            }, 400);
+            $("#iconBox, #iconBox img").animate({
+                height: "-=20vh",
+                width: "-=20vh",
+            }, 400);
+        };
+        // smooth scroll
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 400, 'linear');
+        clickOpenCount = 0;
     })
 
     //for manipulating albums at #works section
@@ -29,10 +66,10 @@ $(function () {
             for (i = 0; i < imgLen; i++) {
                 if ($(`#album${alpha} .albumContent img:nth-child(${i + 1})`).attr("alt") === n) {
                     console.log($(`#album${alpha} .albumContent img:nth-child(${i + 1})`).attr("alt"));
-                    $(`#album${alpha} .albumContent img:nth-child(${i + 1})`).show();
+                    $(`#album${alpha} .albumContent img:nth-child(${i + 1})`).fadeIn("slow");
                 } else {
                     console.log($(`#album${alpha} .albumContent img:nth-child(${i + 1})`).attr("alt"));
-                    $(`#album${alpha} .albumContent img:nth-child(${i + 1})`).hide();
+                    $(`#album${alpha} .albumContent img:nth-child(${i + 1})`).fadeOut("slow");
                 }
             }
         }
@@ -40,7 +77,6 @@ $(function () {
         $(`#album${alpha} .bigOverlay`).click(function () {
             $(`#album${alpha}`).css("display", "none");
             $(`#album${alpha} .button button`).remove();
-            $("body").css("overflow", "auto");
         });
 
         $(`#${alpha}`).click(function () {
@@ -52,7 +88,6 @@ $(function () {
                 )
             }
             imgHS(1);
-            $("body").css("overflow", "hidden");
         }
         )
 
@@ -61,47 +96,31 @@ $(function () {
         })
     }
 
-    allAlbum("a");allAlbum("b");allAlbum("c");allAlbum("d");allAlbum("e");allAlbum("f");allAlbum("g");
-    
-    //customized hover effect at #work section
-    $(".overlay").on("mouseover", function(){
+    allAlbum("a"); allAlbum("b"); allAlbum("c"); allAlbum("d"); allAlbum("e"); allAlbum("f"); allAlbum("g");
+
+    //hover effect at #work section
+    $(".overlay").on("mouseover", function () {
         $(this).fadeOut(500);
-    }).on("mouseleave", function(){
+    }).on("mouseleave", function () {
         $(this).fadeIn(500);
     });
 
-    //effects at contact section
-    $(".pitch").on("mouseenter", function(){
+    //hover effects at contact section
+    $(".pitch").on("mouseenter", function () {
         $("#pitch").fadeOut("fast");
         $(".icon").fadeIn("fast");
+    }).on("mouseleave", function () {
+        $(".icon").fadeOut("fast");
+        $("#pitch").fadeIn("fast");
     });
 
-    // $("#pitch, .pitch").on("mouseout", function(){
-    //     $(this).fadeIn(500);
-    //     setTimeout(function(){
-    //         $(this).css("z-index", "2");
-    //         $(".icon").css("display", "none");
-    //     }, 700)
-    // });
-
-    $(".icon a").on("mouseover", function(){
-        console.log ("mouseover working")
+    $("#icon a").on("mouseover", function () {
+        console.log("mouseover working")
         $(this).animate({
             fontSize: "6vw",
-        }, 300);        
-    });
-    
-    // $(".icon").on("mouseover", function(){
-    //     $("#pitch").css("display", "none")
-    // })
-
-    $(".pitch").on("mouseleave", function(){
-        $(".icon").fadeOut("fast");
-        $("#pitch").fadeIn("")
-    })
-
-    $(".icon a").on("mouseleave", function(){
-        console.log ("mouseover working")
+        }, 300);
+    }).on("mouseleave", function () {
+        console.log("mouseover working")
         $(this).animate({
             fontSize: "4vw",
         }, 300)
