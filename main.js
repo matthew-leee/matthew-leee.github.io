d8a265f3b65e94cc21787098edf782127ba9be61
matthew-leee <psynergy.lth@gmail.com>
2018-10-15 10:46:03 +0800

    small amendment

--- main.js
+++ main.js
@@ -1,61 +1,118 @@
 $(function () {
 
-    //navBar Hide and Show
-    $(window).on("scroll", function (e) {
-        let navOld = $(this).scrollTop();
-        console.log("scroll working")
-        console.log(navOld);
-        let navNew;
-        setTimeout(function (e) {
-            navNew = $(this).scrollTop();
-            //scrollDown or stop
-            if (navNew >= navOld && navNew > 100) {
-                $("nav").css("background-color", "rgba(255,255,255, 0)")
-                $(".nav-link").css("text-shadow", "0 0 7px #FFFFFF")
-                // scrollup or stay at top 
-            } else if (navNew < navOld || navNew <= 100) {
-                $("nav").css("background-color", "rgba(255,255,255, 0.5)")
-                $(".nav-link").css("text-shadow", "0 0 0")
+    if ($(window).width() > 768) {
+        $("#navbarMenu").css("display", "block");
+        //navBar Hide and Show
+        $(window).on("scroll", function (e) {
+            let navOld = $(this).scrollTop();
+            console.log("scroll working")
+            console.log(navOld);
+            let navNew;
+            setTimeout(function (e) {
+                navNew = $(this).scrollTop();
+                //scrollDown or stop
+                if (navNew >= navOld && navNew > 100) {
+                    $("nav").css("background-color", "rgba(255,255,255, 0)")
+                    $(".nav-link").css("text-shadow", "0 0 7px #FFFFFF")
+                    // scrollup or stay at top 
+                } else if (navNew < navOld || navNew <= 100) {
+                    $("nav").css("background-color", "rgba(255,255,255, 0.5)")
+                    $(".nav-link").css("text-shadow", "0 0 0")
+                }
+            }, 100);
+        })
+
+        //#navbar logo opens on click
+        let clickOpenCount = 0;
+        $("#iconBox img").on("click", function () {
+            clickOpenCount++;
+            if (clickOpenCount <= 1) {
+                $("body").css("overflow", "hidden");
+                $("#nav").animate({
+                    height: "+=75vh",
+                    backgroundColor: "rgba(255,255,255, 0.7)",
+                }, 400);
+                $("#iconBox, #iconBox img").animate({
+                    height: "+=20vh",
+                    width: "+=20vh",
+                }, 400);
             }
-        }, 100);
-    })
+        })
 
-    //#navbar logo opens on click
-    let clickOpenCount = 0;
-    $("#iconBox img").on("click", function () {
-        clickOpenCount++;
-        if (clickOpenCount <= 1) {
+        $(".nav-link").on("click", function () {
+            //navbar collapse
+            if (clickOpenCount == 1) {
+                $("body").css("overflow", "auto");
+                $("#nav").animate({
+                    height: "-=75vh",
+                    backgroundColor: "rgba(255,255,255, 0.5)",
+                }, 400);
+                $("#iconBox, #iconBox img").animate({
+                    height: "-=20vh",
+                    width: "-=20vh",
+                }, 400);
+            };
+            // smooth scroll
+            $('html, body').animate({
+                scrollTop: $($(this).attr('href')).offset().top
+            }, 400, 'linear');
+            clickOpenCount = 0;
+        })
+
+    } else if ($(window).width() <= 768){
+        $("#navbarMenu").css("display", "none");
+        let mclickOpenCount = 0;
+        $("#iconBox img").on("click", function () {
             $("body").css("overflow", "hidden");
-            $("#nav").animate({
-                height: "+=75vh",
-                backgroundColor: "rgba(255,255,255, 0.7)",
-            }, 400);
-            $("#iconBox, #iconBox img").animate({
-                height: "+=20vh",
-                width: "+=20vh",
-            }, 400);
-        }
-    })
+            mclickOpenCount++;
+            console.log("clicked")
+            if (mclickOpenCount == 1) {
+                $("#nav").animate({
+                    height: "+=20vh",
+                }, 400);
+                $("#iconBox").after(`
+                <div id="mNavbarMenu">
+                    <ul class="navbar-nav mr-auto" id="mNavbar-ul">
+                        <li class="nav-item">
+                            <a href="#about" class="nav-link">ABOUT</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#works" class="nav-link">WORKS</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#contact" class="nav-link">CONTACT</a>
+                        </li>
+                    </ul>
+                </div>
+                `)
+                $("#mNavbarMenu").css({
+                    "position": "absolute",
+                    "align-self": "flex-start",
+                })
+            }
+        })
 
-    $(".nav-link").on("click", function () {
-        //navbar collapse
-        if (clickOpenCount == 1) {
-            $("body").css("overflow", "auto");
-            $("#nav").animate({
-                height: "-=75vh",
-                backgroundColor: "rgba(255,255,255, 0.5)",
-            }, 400);
-            $("#iconBox, #iconBox img").animate({
-                height: "-=20vh",
-                width: "-=20vh",
-            }, 400);
-        };
-        // smooth scroll
-        $('html, body').animate({
-            scrollTop: $($(this).attr('href')).offset().top
-        }, 400, 'linear');
-        clickOpenCount = 0;
-    })
+        $(".nav-link").on("click", function () {
+            //navbar collapse
+            console.log ("good");
+            if (mclickOpenCount == 1) {
+                $("body").css("overflow", "auto");
+                console.log ("good god")
+                $("#nav").animate({
+                    height: "-=20vh",
+                    backgroundColor: "rgba(255,255,255, 0.5)",
+                }, 400);
+                // smooth scroll
+                $('html, body').animate({
+                    scrollTop: $($(this).attr('href')).offset().top
+                }, 400, 'linear');
+                $("#mNavbarMenu").remove();
+                console.log (good)
+                clickOpenCount = 0;
+            };
+
+        })
+    }
 
     //for manipulating albums at #works section
     function allAlbum(alpha) {
@@ -125,5 +182,6 @@ $(function () {
             fontSize: "4vw",
         }, 300)
     });
+
 
 })
